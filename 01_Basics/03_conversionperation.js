049111de6fa729642ade62319ee8a9842d61f431
Manish Yadav <pcmb202122@gmail.com>
2025-07-24 18:00:16 +0000

    operations

--- 01_Basics/03_conversionperation.js
+++ 01_Basics/03_conversionperation.js
@@ -24,6 +24,40 @@ let isLoggedIn ="manish"
  let someNumber = 33
 
  let stringNumber= String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+ 
+
+ //************************************. OPERATIONS ************************** */
+
+
+ let value = 3
+ let negvalue = -value
+ //console.log(negvalue);
+ 
+ //console.log(2+2) addition 
+ //console.log(2-2) subtraction
+//console.log(2*2). multiplication
+//console.log(2**2);  power
+//console.log(2/3); divide
+//console.log(2%3); modulo
+
+let str1 = "hello"
+let str2 = " hello"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
+
+
+
+
  
