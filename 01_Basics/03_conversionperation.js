76fc9682b31c012ce0d7871535a012f2f5cc1942
Manish Yadav <pcmb202122@gmail.com>
2025-07-24 10:36:12 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionperation.js
@@ -0,0 +1,29 @@
+let score = "manish"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false=> 0
+
+let isLoggedIn ="manish"
+ //let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanIsLoggedIn);
+ 
+ // 1 => true ; 0 => false
+ //" => false
+ // "Manish" =. true
+
+ let someNumber = 33
+
+ let stringNumber= String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
