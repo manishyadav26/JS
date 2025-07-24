e835321ae17b4d4b584118f51fbfd6cf61724e68
Manish Yadav <pcmb202122@gmail.com>
2025-07-24 10:17:56 +0000

    variables and data types

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=14453
+let accountEmail="pcmb2021222gmail.com"
+var accountPassword="123456"
+accountCity="Jaipur"// this can be written but its not standard
+let accountState;
+
+//accountId=2// not allowed 
+
+accountEmail="pcmb@gmail.com"
+accountCity="Mathura"
+accountPassword="4353672"
+
+/* Prefer not to use var 
+because of issue in block scope and functional scope*/ 
+console.log(accountId);
+console.table([accountCity, accountPassword, accountCity, accountId, accountState])
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"//treats all JS code as newer version
+
+//alert(3+3)// we are using node.js not web browser
+
+console.log(3+
+    3)  // code readability should be high
+
+console.log("Manish")
+
+let name = "manish"
+let age = 21
+let isLoggedIn= false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/falser\
+//null => stand alone value 
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
+
